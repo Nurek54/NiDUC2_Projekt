e85c9c83a027d71d4c5f5f0886295622de420be4
Nurek54 <g_sienkiewicz@o2.pl>
2024-03-18 15:21:19 +0100

    Initial Commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
-#include <iostream>
+#include <bits/stdc++.h>+
+using namespace std;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << endl;
     return 0;
 }
